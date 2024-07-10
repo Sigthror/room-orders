0e3f83e2ef5297df8d7bfe51bfba0c682989bc47
Artem Mezentsev <mezentzeff.artyom@gmail.com>
2024-07-10 09:28:55 +0300

    refactor: error handling from repository

--- internal/model/order.go
+++ internal/model/order.go
@@ -3,9 +3,9 @@ package model
 import "time"
 
 type Order struct {
-	HotelID   string    `json:"hotel_id"`
-	RoomID    string    `json:"room_id"`
-	UserEmail string    `json:"email"`
-	From      time.Time `json:"from"`
-	To        time.Time `json:"to"`
+	HotelID   string    `json:"hotel_id" validate:"required"`
+	RoomID    string    `json:"room_id" validate:"required"`
+	UserEmail string    `json:"email" validate:"required,email"`
+	From      time.Time `json:"from" validate:"required"`
+	To        time.Time `json:"to" validate:"required,gtcsfield=From"`
 }
